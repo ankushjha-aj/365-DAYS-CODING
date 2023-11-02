0d1b19b7d941d3bb4cc379e498ca55a2f7f623ac
Ankush Kumar Jha <jhaankushcpp@gmail.com>
2023-11-03 00:26:14 +0530

    Create Airthematic_operations.py

---
+++ Day_1/Airthematic_operations.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
+put numbers instead of int(input()), as it is a hackerrank problem
