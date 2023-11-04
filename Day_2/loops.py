675b5c846f0dc323f98505c628e95307c0e98af6
Ankush Kumar Jha <jhaankushcpp@gmail.com>
2023-11-04 07:31:24 +0530

    Create loops.py

---
+++ Day_2/loops.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i*i)
+
+  # you can change values as it is a hackerrank question 
+  # it will print the square of all numbers which is less than n 
